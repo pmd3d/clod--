44bd1b6a4760035735c88ab8db5fcc80fbb6f330
Jonathan Lim <jjrr287@gmail.com>
2026-02-14 21:13:13 -0800

    checkin draft ocaml for conversion of address_taken functions

--- src/optimizations/address_taken.fs
+++ src/optimizations/address_taken.fs
@@ -1,3 +1,19 @@
-module Address_taken
+module Address_taken = Set.Make (String) // convert ocaml to F#
 
-let analyze_program _ = Set.empty<string>
+// convert ocaml to F#
+let analyze instrs =
+  let addr_taken = function
+    | Tacky.GetAddress { src = Var v; _ } -> Some v
+    | _ -> None
+  in
+
+  StringSet.of_list (List.filter_map addr_taken instrs)
+
+// convert ocaml to F#
+let analyze_program (Tacky.Program tls) =
+  let analyze_tl = function
+    | Tacky.Function f -> analyze f.body
+    | _ -> StringSet.empty
+  in
+  let aliased_vars_per_fun = List.map analyze_tl tls in
+  List.fold_left StringSet.union StringSet.empty aliased_vars_per_fun
